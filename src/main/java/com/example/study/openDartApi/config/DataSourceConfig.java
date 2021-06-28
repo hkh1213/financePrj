ea5d74d0a62481099921185eba74df6682f90944
hkh1213 <64997271+hkh1213@users.noreply.github.com>
2021-06-28 09:20:08 +0900

    Add files via upload
    adad

--- src/main/java/com/example/study/openDartApi/config/DataSourceConfig.java
+++ src/main/java/com/example/study/openDartApi/config/DataSourceConfig.java
@@ -13,6 +13,17 @@ public class DataSourceConfig {
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+        String username="admin";
+        String password="12131213";
+        String jdbcUrl="jdbc:mysql://database-3.crvzrkxtwlmg.ap-northeast-2.rds.amazonaws.com:3306/spring";
+        String driverClass="com.mysql.jdbc.Driver";
+
+        DataSourceBuilder dataSourceBuilder=DataSourceBuilder.create();
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+        dataSourceBuilder.url(jdbcUrl);
+        dataSourceBuilder.driverClassName(driverClass);
+
+        return dataSourceBuilder.build();
     }
 }
